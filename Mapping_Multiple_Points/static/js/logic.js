4e529d74eea78c50b4e7495aae4a0dc13326e94d
Mac User <macuser@MacintoshUsers-MacBook-2.local>
2020-03-17 18:52:35 -0700

    Adding mapping multiple pointfiles

--- Mapping_Multiple_Points/static/js/logic.js
+++ Mapping_Multiple_Points/static/js/logic.js
@@ -4,6 +4,9 @@ console.log("working");
 // Create the map object with a center and zoom level.
 let map = L.map('mapid').setView([40.7, -94.5], 4);
 
+// Get data from cities.js
+let cityData = cities;
+
 // Create the map object with a center and zoom level.
 /*let map = L.map("mapid", {
     center: [
@@ -13,7 +16,21 @@ let map = L.map('mapid').setView([40.7, -94.5], 4);
   });*/
 
 //  Add a marker to the map for Los Angeles, California.
-let marker = L.marker([34.0522, -118.2437]).addTo(map);
+//let marker = L.marker([34.0522, -118.2437]).addTo(map);
+
+// Loop through the cities array and create one marker for each city.
+cityData.forEach(function(city) {
+  console.log(city)
+  L.circleMarker(city.location, {
+    color: 'orange',
+    fillColor: '#ffffa1',
+    radius: city.population/100000
+  })
+  //L.marker(city.location).addTo(map)
+  .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
+  .addTo(map);
+ });
+
 
 /*L.circle([34.0522, -118.2437], {
   color: 'black',
@@ -21,11 +38,11 @@ let marker = L.marker([34.0522, -118.2437]).addTo(map);
   radius: 300
 }).addTo(map);*/
 
-L.circleMarker([34.0522, -118.2437],{
-  color: 'black',
+/*L.circleMarker([34.0522, -118.2437],{
+  or: 'black',
   fillColor: '#ffffa1',
   radius: 300
-}).addTo(map);
+}).addTo(map);*/
 
 
 // We create the tile layer that will be the background of our map.
